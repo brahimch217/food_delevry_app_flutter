c45299e7a93af5bfa62f6f0ae0a0459d6ec5d241
ibrahim cherifi <69094349+brahimch217@users.noreply.github.com>
2022-06-19 08:23:38 +0200

    trs

---
+++ sellers_app/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:sellers_food_app/global/global.dart';
+import 'package:sellers_food_app/screens/home_screen.dart';
+import 'package:sellers_food_app/splash_screen/splash_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  sharedPreferences = await SharedPreferences.getInstance();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: "AIzaSyDnpzELhs49KijDxz1UJ4ukA2gbmNnUl5w",
+    appId: "1:21956223765:web:1c404a6e9c7b88fa077f1c",
+    messagingSenderId: "21956223765",
+    projectId: "tresorie-d8c94",
+    storageBucket: "tresorie-d8c94.appspot.com",
+  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sellers App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
